18a1bfe366c6c1b9b7c92f9e8f06493345d3b6e6
Artem Slabodnik <artem.slabodnik@gmail.com>
2014-11-10 20:30:05 +0300

    test angular page for resume

---
+++ assets/js/app.js
@@ -0,0 +1,40 @@
+(function(){
+	var app = angular.module('gemStore', []);
+
+	app.controller("StoreController", function(){
+		this.products = gems;
+	});
+
+	app.controller("TabController", function(){
+		this.tab = 1;
+
+		this.selectTab = function(setTab){
+			this.tab = setTab;
+		};
+		this.isSelected = function(isSet){
+			return this.tab === isSet;
+		};
+	});
+
+	app.controller("GalleryController", function(){
+		this.current = 0;
+
+		this.setCurrent = function(newGallery){
+			this.current = newGallery || 0;
+		};
+	});
+
+	app.controller("ReviewController", function(){
+		this.review = { };
+
+		this.addReview = function(product){
+			this.review.createdOn = Date.now();
+			product.reviews.push(this.review);
+			this.review = {};
+		};
+
+		this.removeReview = function(product, index){
+			product.reviews.splice(index, 1);
+		};
+	});
+})();
---
+++ assets/js/variables.js
@@ -0,0 +1,43 @@
+var gems = [{
+  name: 'Azurite',
+  description: "Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.",
+  shine: 8,
+  price: 110.50,
+  rarity: 7,
+  color: '#CCC',
+  faces: 14,
+  images: [
+    "images/shot-1.png",
+    "images/shot-2.png",
+    "images/shot-3.png"
+  ],
+  reviews: [{stars: 5, description: "dfjnlfjhgfvs", author: "sdf@df.df"}]
+}, {
+  name: 'Bloodstone',
+  description: "Origin of the Bloodstone is unknown, hence its low value. It has a very high shine and 12 sides, however.",
+  shine: 9,
+  price: 22.90,
+  rarity: 6,
+  color: '#EEE',
+  faces: 12,
+  images: [
+    "images/shot-4.png",
+    "images/shot-5.png",
+    "images/shot-6.png"
+  ],
+  reviews: []
+}, {
+  name: 'Zircon',
+  description: "Zircon is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.",
+  shine: 70,
+  price: 1100,
+  rarity: 2,
+  color: '#000',
+  faces: 6,
+  images: [
+    "images/shot-1.png",
+    "images/shot-2.png",
+    "images/shot-3.png"
+  ],
+  reviews: []
+}];
